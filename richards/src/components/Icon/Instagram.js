c0ad338239c1644c89fbaaec26fe642099538112
colin <colin.rhys.richards@gmail.com>
2019-01-15 20:22:12 -0700

    Tab section styling and starting to fill in the text

---
+++ richards/src/components/Icon/Instagram.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Instagram.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Instagram = () => (
+<div className="elfsight-app-f0fb39e2-e88a-4a78-8872-02d8989c544e" id="instafeed"/>
+
+);
+
+export default Instagram;
