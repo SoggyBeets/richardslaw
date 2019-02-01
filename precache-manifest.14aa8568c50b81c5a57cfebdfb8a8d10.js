d0acf17e6aaea354e716521f4e2d32e8deaa1e81
colin <colin.rhys.richards@gmail.com>
2019-01-31 19:35:11 -0700

    Updates

---
+++ precache-manifest.14aa8568c50b81c5a57cfebdfb8a8d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b830e2d0b8f160c59a9",
+    "url": "/richardslaw/static/js/runtime~main.8b830e2d.js"
+  },
+  {
+    "revision": "adc449bf2f132996e697",
+    "url": "/richardslaw/static/js/main.adc449bf.chunk.js"
+  },
+  {
+    "revision": "1e19bebb8568e0027f29",
+    "url": "/richardslaw/static/js/1.1e19bebb.chunk.js"
+  },
+  {
+    "revision": "adc449bf2f132996e697",
+    "url": "/richardslaw/static/css/main.327ed3fd.chunk.css"
+  },
+  {
+    "revision": "1e19bebb8568e0027f29",
+    "url": "/richardslaw/static/css/1.74033ed1.chunk.css"
+  },
+  {
+    "revision": "396d6f63a4bbfae5f380aee71d043d0d",
+    "url": "/richardslaw/index.html"
+  }
+];
