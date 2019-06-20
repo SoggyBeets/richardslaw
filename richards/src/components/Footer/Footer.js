0b5abd95b87573461a0d735d72516b8be46b6362
colin richards <colin.rhys.richards@gmail.com>
2019-06-19 19:25:23 -0600

    footer links, attorney card,

--- richards/src/components/Footer/Footer.js
+++ richards/src/components/Footer/Footer.js
@@ -68,7 +68,7 @@ const ourLogo = {
 };
 
 const fb = {
-  fontSize: "2.25rem",
+  fontSize: "2.15rem",
   color: "white",
   padding: "10px",
   marginRight: "2rem",
@@ -99,20 +99,26 @@ const Footer = () => (
         <Row noGutters>
           <Col md="2" />
           <Col md="2">
+          <div className="footerContactSections">
             <h2 id="contact">Address</h2>
             <h3 href="https://www.google.com/maps/dir/41.2179494,-111.9417437/rbr+law/@41.2203729,-111.9752583,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x87530f2088890a47:0xde0ebd0671a6d4e4!2m2!1d-111.9737615!2d41.2224332"
                id="contactText">
                289 24th St, Ogden, UT 84401</h3>
+               </div>
           </Col>
           <Col md="1" />
           <Col md="2">
+          <div className="footerContactSections">
             <h2 id="contact">Contact Us</h2>
-            <h3 href="tel:+1-801-621-7443" id="contactText">(801) 621-7443</h3>
+            <a href="tel:+1-801-621-7443" id="contactText">(801) 621-7443</a>
+            </div>
           </Col>
           <Col md="1" />
           <Col md="2">
+            <div className="footerContactSections">
             <h2 id="contact">Mail Us</h2>
-            <h3 id="contactText">example@example.mail</h3>
+            <a href="mailto:example@example.mail" id="contactText">example@example.mail</a>
+            </div>
           </Col>
           <Col md="2" />
         </Row>
@@ -131,9 +137,9 @@ const Footer = () => (
           <Col md="4" />
           <Col md="4">
             <div style={iconLinks}>
-              <FontAwesomeIcon icon={["fab", "google"]} style={google} />
-              <FontAwesomeIcon icon={["fab", "facebook"]} style={fb} onClick="parent.open('https://www.facebook.com/RBRlawgroup/')" />
-              <FontAwesomeIcon icon={["fab", "instagram"]} style={insta} />
+            <a href="https://www.google.com/search?rlz=1C1CHBF_enUS843US843&ei=mcUKXba6O4W0tQXGrZbgBw&q=rbr+law+group&oq=rbr+law+group&gs_l=psy-ab.3..35i39j0.21256.23176..24034...0.0..0.108.1255.8j5......0....1..gws-wiz.......0i71j0i131j0i67j0i20i263j0i10j0i22i30.GqaiAcmF8SU"><FontAwesomeIcon icon={["fab", "google"]} style={google} /></a>
+             <a href="https://www.facebook.com/RBRlawgroup/"><FontAwesomeIcon icon={["fab", "facebook"]} style={fb} /></a>
+             <a href="https://www.instagram.com/explore/locations/291504041479/richards-brinley-richards-law-group/?hl=en"><FontAwesomeIcon icon={["fab", "instagram"]} style={insta} /></a>
             </div>
           </Col>
           <Col md="4" />
@@ -141,13 +147,13 @@ const Footer = () => (
 
         <Row noGutters>
           <Col md="12">
-            <div id="ourLogoSection">
+            {/* <div id="ourLogoSection">
               <h3 style={copyrightStyle}>
                 Created By
                 <FontAwesomeIcon icon="carrot" style={ourLogo} />
                 SoggyBeets.
               </h3>
-            </div>
+            </div> */}
           </Col>
         </Row>
       </Col>
